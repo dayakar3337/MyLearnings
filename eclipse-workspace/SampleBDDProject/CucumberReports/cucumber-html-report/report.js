0702c22e7fd3bedd9faa7ae4a3d92b08aa1466a7
Dayakar <dayar.qa@gmail.com>
2020-07-30 18:55:50 -0500

    first commit

---
+++ eclipse-workspace/SampleBDDProject/CucumberReports/cucumber-html-report/report.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/DRetineni/eclipse-workspace/SampleBDDProject/src/main/java/Features/TestSite.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Test Site Feature",
+  "description": "",
+  "id": "test-site-feature",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Testind Demo site",
+  "description": "",
+  "id": "test-site-feature;testind-demo-site",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "I\u0027m on TestSite home page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I click on Elements icon",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "I Click on Text Box icon",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I enter the required details",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "I  click on Submit button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
---
+++ eclipse-workspace/SampleBDDProject/target/classes/Features/LoginTest.feature
@@ -0,0 +1,11 @@
+Feature: Chrome Test Feature 
+
+ 
+Scenario: Chrome browser test  
+
+Given User launched Chrome browser
+When User open Google Home page
+Then I verify the the page displays search text box
+
+ 
+
